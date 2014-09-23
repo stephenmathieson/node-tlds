eac247980eac21f13fbbd4db96b4b4be20788f9c
Nathan Rajlich <nathan@tootallnate.net>
2014-09-23 13:36:36 -0700

    cron: use local bin variables

--- cron.sh
+++ cron.sh
@@ -14,8 +14,8 @@ DIRTY=$?
 if [ DIRTY=1 ]; then
   git add index.js
   git commit -m "index: update list from ICANN"
-  VERSION=$(node -p "require('./package').version")
-  INCREMENT=$(semver --increment minor $VERSION)
+  VERSION=$($NODE -p "require('./package').version")
+  INCREMENT=$($SEMVER --increment minor $VERSION)
   git release $INCREMENT
   npm publish
 fi;
